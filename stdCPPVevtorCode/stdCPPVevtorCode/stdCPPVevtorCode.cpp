6f6b064e9fdca7e7800583bbc8e742c2a71179ef
user595 <dwightwilkins@gmail.com>
2014-02-11 13:23:59 -0600

    starting

---
+++ stdCPPVevtorCode/stdCPPVevtorCode/stdCPPVevtorCode.cpp
@@ -0,0 +1,14 @@
+// stdCPPVevtorCode.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <vector>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	using namespace std;
+	vector<int> book(1,2);//parms:size,element
+
+	return 0;
+}
+
